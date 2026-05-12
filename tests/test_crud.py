8d9faf8045f497ca4605eec6d0c354540f3e7d76
Austin Angelus <austin.angelus@gmail.com>
2026-05-12 16:54:10 -0700

    Add more type annotations to tests/test_crud.py

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -12,13 +12,13 @@ class TestPostsCRUD:
         assert isinstance(data, list)
         assert len(data) == 100
 
-    def test_get_post_by_id(self, client: APIClient, existing_post) -> None:
+    def test_get_post_by_id(self, client: APIClient, existing_post: dict) -> None:
         assert existing_post["id"] == 1
         assert "title" in existing_post
         assert "body" in existing_post
         assert "userId" in existing_post
 
-    def test_create_post(self, client: APIClient, new_post_payload) -> None:
+    def test_create_post(self, client: APIClient, new_post_payload: dict) -> None:
         resp = client.post("/posts", json=new_post_payload)
         assert resp.status_code == 201
         body = resp.json()
